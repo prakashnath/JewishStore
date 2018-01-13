d169896943441e204b6cfc330887154d3a417162
Chaim Greenberg <chaim8greenberg@gmail.com>
2018-01-13 19:42:18 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,69 @@
+module.exports = function() {
+    return {
+        products: [{
+                id: 1,
+                name: "Tphillin Ashkenaz",
+                category: "Tphillins",
+                description: "A kosher tphillin - ashkenazi nusah",
+                price: 1999
+            },
+            {
+                id: 2,
+                name: "Tphillin Spharad",
+                category: "Tphillins",
+                description: "A kosher tphillin - spharadi nusah",
+                price: 1999.95
+            },
+            {
+                id: 3,
+                name: "Talit gadol ragil",
+                category: "Talits",
+                description: "Talit gadol white color",
+                price: 99.99
+            },
+            {
+                id: 4,
+                name: "Talit gadol hazelet",
+                category: "Talits",
+                description: "Talit gadol white color with cyan stripes",
+                price: 89.99
+            },
+            {
+                id: 5,
+                name: "Talit katan",
+                category: "Talits",
+                description: "Talit katan universal size",
+                price: 29.95
+            },
+            {
+                id: 6,
+                name: "Tora",
+                category: "Books",
+                description: "Tora with Rashi comments translated in Russian",
+                price: 16
+            },
+            {
+                id: 7,
+                name: "Sidur Ashkenaz",
+                category: "Books",
+                description: "Sidur Ashkenaz with Russian translation",
+                price: 29.95
+            },
+            {
+                id: 8,
+                name: "Sidur Spharad",
+                category: "Books",
+                description: "Sidur Spharad with Russian translation",
+                price: 75
+            },
+            {
+                id: 9,
+                name: "Sidur Ari",
+                category: "Books",
+                description: "Sidur Ari (HABAD edition) with Russian translation",
+                price: 1200
+            }
+        ],
+        orders: []
+    }
+}
